0ae2006b25bd388495c171208201a4d9d9bfc34f
Lucas Henry <lhenry@bizstream.com>
2024-05-22 09:56:30 -0400

    ask button removed unused

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import Navbar from './components/navbar/navbar.tsx'
 import Resume from './components/resume/resume.tsx'
 import Chatbot from './chatbot/chatbot.tsx'
 
