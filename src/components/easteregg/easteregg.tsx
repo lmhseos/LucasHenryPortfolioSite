e9d6d9700e4d34140ce7935a1cd6172553fd18b2
Lucas Henry <lmhseos@gmail.com>
2025-04-04 14:39:10 -0400

    fixed build 2

--- src/components/easteregg/easteregg.tsx
+++ src/components/easteregg/easteregg.tsx
@@ -1,36 +1,36 @@
-//import React, { useState } from 'react';
-import Modal from 'react-modal';
-import './easteregg.css';
+// //import React, { useState } from 'react';
+// import Modal from 'react-modal';
+// import './easteregg.css';
 
-Modal.setAppElement('#root');
+// Modal.setAppElement('#root');
 
-function App() {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+// function App() {
+//   const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  const openModal = () => {
-    setModalIsOpen(true);
-  };
+//   const openModal = () => {
+//     setModalIsOpen(true);
+//   };
 
-  const closeModal = () => {
-    setModalIsOpen(false);
-  };
+//   const closeModal = () => {
+//     setModalIsOpen(false);
+//   };
 
-  return (
-    <div className="App">
-      <button className="duck-button" onClick={openModal}></button>
+//   return (
+//     <div className="App">
+//       <button className="duck-button" onClick={openModal}></button>
 
-      <Modal 
-        isOpen={modalIsOpen} 
-        onRequestClose={closeModal}
-        contentLabel="Image Modal"
-        className="modal-content"
-        overlayClassName="modal-overlay"
-      >
-        <span className="close" onClick={closeModal}>&times;</span>
-        <img src="https://via.placeholder.com/150" alt="Model Image" />
-      </Modal>
-    </div>
-  );
-}
+//       <Modal 
+//         isOpen={modalIsOpen} 
+//         onRequestClose={closeModal}
+//         contentLabel="Image Modal"
+//         className="modal-content"
+//         overlayClassName="modal-overlay"
+//       >
+//         <span className="close" onClick={closeModal}>&times;</span>
+//         <img src="https://via.placeholder.com/150" alt="Model Image" />
+//       </Modal>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
